3af7120b079c60f5eb02f89fa9e8f1daf8bd13e7
Marika Ohlsson <marikaeohlsson@gmail.com>
2019-01-30 14:13:04 +0100

    Opened project with movement

---
+++ Missed-Game/Assets/movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Rigidbody2D body;
+    public float speed;
+
+    void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void Update()
+    {
+        float horizontalSpeed = Input.GetAxisRaw("Horizontal");
+        float verticalSpeed = Input.GetAxisRaw("Vertical");
+
+        body.velocity = new Vector2(horizontalSpeed * speed, verticalSpeed * speed);
+    }
+}
