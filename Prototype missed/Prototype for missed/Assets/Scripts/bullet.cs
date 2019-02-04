aa3c87755039c19e4825818402ba26886a741cf2
Erik Norstedt <eriknorth.swe@gmail.com>
2019-02-04 14:21:03 +0100

    Prototype

---
+++ Prototype missed/Prototype for missed/Assets/Scripts/bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed = 10f;
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            enemyScript enemy = collision.GetComponent<enemyScript>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype missed/Prototype for missed/Assets/Scripts/weapom.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapom : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwShots <= 0)
+        {
+
+            if (Input.GetButton("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        timeBtwShots = startTimeBtwShots;
+    }
+}
