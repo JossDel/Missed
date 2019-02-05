cfdfb20059eceb3ff676621cbc79bf54eb29b114
Unknown <joseph@delatolas.se>
2019-02-05 16:11:54 +0100

    Added Stats and cleared the movement script

---
+++ Prototype missed/Prototype for missed/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int health = 100;
+    public int corruption = 0;
+    public float movementSpeed = 2.5f;
+    public int activeWeapon = 1;
+    public float dps = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
