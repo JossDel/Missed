35f51b384651167b62222a82a37c7c3a487e59b1
Erik Norstedt <eriknorth.swe@gmail.com>
2019-02-21 09:55:21 +0100

    showing off stages
    the mist has issues in level 1 tho

---
+++ Prototype missed/Prototype for missed/Assets/Scripts/PressentationMIST.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressentationMIST : MonoBehaviour
+{
+    public Transform pointofmist;
+    public GameObject mistprefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Instantiate(mistprefab, pointofmist.position, pointofmist.rotation);
+           
+        }
+    }
+}
