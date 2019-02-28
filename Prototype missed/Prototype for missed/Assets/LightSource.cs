0fe1fdd0f1ce49fc922fbb2c38ae9dcf7a2f77b3
Erik Norstedt <eriknorth.swe@gmail.com>
2019-02-28 13:36:25 +0100

    Made folders and added title screen

---
+++ Prototype missed/Prototype for missed/Assets/LightSource.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSource : MonoBehaviour
+{
+
+    [SerializeField] int healthBack = 10;
+    [SerializeField] float corruptionDown = 10f;
+    
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerStats>().health += healthBack;
+            other.GetComponent<PlayerStats>().corruption -= corruptionDown;
+            Destroy(gameObject);
+        }
+    }
+}
