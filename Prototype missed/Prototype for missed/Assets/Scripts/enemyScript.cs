454733677a574f89266048eef79114de2f0c95a1
Erik Norstedt <eriknorth.swe@gmail.com>
2019-03-03 21:29:07 +0100

    started on firecharge, removed stuff from bullet script

--- Prototype missed/Prototype for missed/Assets/Scripts/enemyScript.cs
+++ Prototype missed/Prototype for missed/Assets/Scripts/enemyScript.cs
@@ -3,16 +3,7 @@
 public class enemyScript : MonoBehaviour {
 
     public int Health = 8;
-    public
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+ 
     public void TakeDamage(int damage)
     {
 
