d3608fee49cfa3a111cb681ab87a1161cd345fb1
Unknown <joseph@delatolas.se>
2019-03-03 21:51:21 +0100

    changed mat for everything on the grid, and added a light

--- Prototype missed/Prototype for missed/Assets/secondmistmovement.cs
+++ Prototype missed/Prototype for missed/Assets/secondmistmovement.cs
@@ -6,19 +6,11 @@ public class secondmistmovement : MonoBehaviour
 {
     public float speed = 0.5f;
 
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
 
-    public GameObject secondmist;
-    // Use this for initialization
     void Start()
     {
-        secondmist.SetActive(true);
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * speed;
     }
-    private void Update()
-    {
-        
-    }
-
 }
