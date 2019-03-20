65fe34f6225ac1da396874897e24b1ab1e8c9ac8
Erik Norstedt <eriknorth.swe@gmail.com>
2019-03-20 14:20:13 +0100

    Mages fireballs are done!

---
+++ Prototype missed/Prototype for missed/Assets/Fireonground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireonground : MonoBehaviour
+{
+    [SerializeField] int damage = 5;
+    public GameObject poof;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerStats player = other.GetComponent<PlayerStats>();
+            if (player != null)
+            {
+                player.takeDamage(damage);
+                Instantiate(poof, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
