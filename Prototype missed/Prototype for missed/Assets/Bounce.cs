dc3abb98540c39b86fdb03dd85df4c90e3e4b08c
Erik Norstedt <eriknorth.swe@gmail.com>
2019-03-20 15:58:35 +0100

    Changed to new sprite for mages
    Working on making its eyes bounce with the animation

---
+++ Prototype missed/Prototype for missed/Assets/Bounce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public Transform Highpoint;
+    public Transform Lowpoint;
+    private bool dirUp = true;
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        if (dirUp)
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        else
+            transform.Translate(-Vector2.up * speed * Time.deltaTime);
+
+       /* if (transform.position.y >= Highpoint.position)
+        {
+            dirUp = false;
+        }
+
+        if (transform.position.x <= Lowpoint)
+        {
+            dirUp = true;
+        } */
+    }
+}
