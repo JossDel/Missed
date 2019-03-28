bfe72c80715d0a2188c17d696fb63aeb0169dd7b
Joss <joseph@delatolas.se>
2019-03-28 12:10:58 +0100

    Player turns

--- Prototype missed/Prototype for missed/Assets/Scripts/Movement.cs
+++ Prototype missed/Prototype for missed/Assets/Scripts/Movement.cs
@@ -7,18 +7,29 @@ public class Movement : PlayerStats
 
     public bool canMove = true;
 
+    Animator animator;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     void Update()
     {
-            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            moveVeocity = moveInput.normalized * movementSpeed;
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVeocity = moveInput.normalized * movementSpeed;
+        if (moveVeocity.x < 0)
+            animator.SetInteger("Direction", 1);
+        else if (moveVeocity.x > 0)
+            animator.SetInteger("Direction", 3);
+        else if (moveVeocity.y < 0)
+            animator.SetInteger("Direction", 0);
+        else if (moveVeocity.y > 0)
+            animator.SetInteger("Direction", 2);
     }
 
 
-void FixedUpdate()
+    void FixedUpdate()
     {
         if (canMove)
             rb.MovePosition(rb.position + moveVeocity * Time.fixedDeltaTime);
